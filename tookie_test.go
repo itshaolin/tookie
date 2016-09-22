48c4e9882a6be198141d23fcfe32d75bccd0514a
luxee <alex@rasklad.com>
2016-09-22 14:35:18 +0300

    franela/goblin => itshaolin/goblin

--- tookie_test.go
+++ tookie_test.go
@@ -4,7 +4,7 @@ package tookie
 
 import (
     "testing"
-    "github.com/franela/goblin"
+    "github.com/itshaolin/goblin"
 )
 
 func TestTools(t *testing.T) {
@@ -19,16 +19,12 @@ func TestTools(t *testing.T) {
         })
 
         g.It("no rest error", func() {
-            defer func() {
-                g.Assert(recover() != nil).IsTrue()
-            }()
+            defer g.AssertPanic()
             consumeFieldV2("3:zxc")
         })
 
         g.It("malformed field error", func() {
-            defer func() {
-                g.Assert(recover() != nil).IsTrue()
-            }()
+            defer g.AssertPanic()
             consumeFieldV2("5:zxc|Tail")
         })
     })
@@ -39,13 +35,13 @@ func TestDecode(t *testing.T) {
 
     g.Describe("#decodeFields", func() {
         //skey := "secure_key"
-    //
-    //    g.It("decodes Tornado v.2 secured cookie", func() {
-    //        enc := "2|1:0|10:1474429838|4:some|8:dmFsdWU=|bf3d274f65723ddb8ce27c74b1c5fa76def0bffa27b583f2887bf6c7e9fc4016"
-    //        ver, name, dec := decodeFields(enc)
-    //        g.Assert(dec).Equal("value")
-    //    })
-    //
+        //
+        //    g.It("decodes Tornado v.2 secured cookie", func() {
+        //        enc := "2|1:0|10:1474429838|4:some|8:dmFsdWU=|bf3d274f65723ddb8ce27c74b1c5fa76def0bffa27b583f2887bf6c7e9fc4016"
+        //        ver, name, dec := decodeFields(enc)
+        //        g.Assert(dec).Equal("value")
+        //    })
+        //
         g.It("decodes Tornado v.2 secured cookie #2", func() {
             enc := "2|1:0|10:1474488231|1:2|8:U2Vjb25k|accec3cbe6fea20fb48cafafe2e77c459ca31b9ae7d83b6f4f6c93e26fa4d85b"
             c, err := decodeFieldsV2(enc)
