32a90a44ba481bafa21e3f6034b1a89ddf0da400
luxee <alex@rasklad.com>
2016-09-22 13:01:10 +0300

    consumeFieldV2 method ready

--- tookie.go
+++ tookie.go
@@ -1,13 +1,57 @@
 // Package tookie provides
-// ...
+// - encoding/decoding of Tornado webserver secure cookies
+// - supports version 2 only
 package tookie
+
+import (
+    "fmt"
+    "strings"
+    "strconv"
+    "errors"
+)
 
 func Decode(secretKey, name, enc string) string {
     return "value"
 }
 
-func decodeFields(en string) (version int, name string, value string) {
-    // key_version, timestamp, name_field, value_field, passed_sig = _decode_fields_v2(value)
+type Cookie struct {
+    Version int
+    Name string
+    Value string
+    Sig string
+}
 
-    return 2, "some", "value"
+func consumeFieldV2(rest string) (v string, r string) {
+    arr := strings.SplitN(rest, ":", 2)
+    ln, err := strconv.Atoi(arr[0])
+    if err != nil {
+        panic(err)
+    }
+    rest = arr[1]
+    v = rest[:ln]
+    if rest[ln] != '|' {
+        panic(fmt.Sprintf("malformed v2 signed value field: %s", rest))
+    }
+    return v, rest[ln + 1:]
+}
+
+func decodeFieldsV2(s string) (c *Cookie, err error) {
+    c = new(Cookie)
+
+    defer func(c *Cookie) (*Cookie, error) {
+        if err := recover(); err != nil {
+            return nil, errors.New("malformed v2 signed value field")
+        }
+        return c, nil
+    }(c)
+
+    version, rest := consumeFieldV2(s[2:])
+    c.Version, err = strconv.Atoi(version)
+    if err != nil {
+        return nil, err
+    }
+    _, rest = consumeFieldV2(rest)
+    c.Name, rest = consumeFieldV2(rest)
+    c.Value, c.Sig = consumeFieldV2(rest)
+    return
 }
--- tookie_test.go
+++ tookie_test.go
@@ -7,11 +7,38 @@ import (
     "github.com/franela/goblin"
 )
 
-func TestInner(t *testing.T) {
-    //g := goblin.Goblin(t)
+func TestTools(t *testing.T) {
+    g := goblin.Goblin(t)
 
-    //g.Describe("#decodeFields", func() {
-    //    skey := "secure_key"
+    g.Describe("#consumeFieldV2", func() {
+
+        g.It("extracts one", func() {
+            v, r := consumeFieldV2("4:asdf|...")
+            g.Assert(v).Equal("asdf")
+            g.Assert(r).Equal("...")
+        })
+
+        g.It("no rest error", func() {
+            defer func() {
+                g.Assert(recover() != nil).IsTrue()
+            }()
+            consumeFieldV2("3:zxc")
+        })
+
+        g.It("malformed field error", func() {
+            defer func() {
+                g.Assert(recover() != nil).IsTrue()
+            }()
+            consumeFieldV2("5:zxc|Tail")
+        })
+    })
+}
+
+func TestDecode(t *testing.T) {
+    g := goblin.Goblin(t)
+
+    g.Describe("#decodeFields", func() {
+        //skey := "secure_key"
     //
     //    g.It("decodes Tornado v.2 secured cookie", func() {
     //        enc := "2|1:0|10:1474429838|4:some|8:dmFsdWU=|bf3d274f65723ddb8ce27c74b1c5fa76def0bffa27b583f2887bf6c7e9fc4016"
@@ -19,13 +46,17 @@ func TestInner(t *testing.T) {
     //        g.Assert(dec).Equal("value")
     //    })
     //
-    //    g.It("decodes Tornado v.2 secured cookie #2", func() {
-    //        enc := "2|1:0|10:1474488231|1:2|8:U2Vjb25k|accec3cbe6fea20fb48cafafe2e77c459ca31b9ae7d83b6f4f6c93e26fa4d85b"
-    //        dec := Decode(skey, "2", enc)
-    //        g.Assert(dec).Equal("Second")
-    //    })
-    //
-    //})
+        g.It("decodes Tornado v.2 secured cookie #2", func() {
+            enc := "2|1:0|10:1474488231|1:2|8:U2Vjb25k|accec3cbe6fea20fb48cafafe2e77c459ca31b9ae7d83b6f4f6c93e26fa4d85b"
+            c, err := decodeFieldsV2(enc)
+            g.Assert(err).Equal(nil)
+            g.Assert(c.Sig).Equal("accec3cbe6fea20fb48cafafe2e77c459ca31b9ae7d83b6f4f6c93e26fa4d85b")
+            g.Assert(c.Version).Equal(0)
+            g.Assert(c.Name).Equal("2")
+            g.Assert(c.Value).Equal("U2Vjb25k")
+        })
+
+    })
 }
 
 func TestTookie(t *testing.T) {
